52f81565e4e44edf7f1664154e32b82fd41c1a93
CristianOjog <cristianconstantin.ojog@gmail.com>
2023-01-30 10:33:39 +0200

    I added an 'onClick' attribute to the 3 buttons, thus simplifying the code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 
 const lastResult = document.querySelector('.lastResult');
-const buttons = document.querySelector('.btn-group');
 const primaryButton = document.querySelector('.primaryButton');
 const secondaryButton = document.querySelector('.secondaryButton');
 const tertiaryButton = document.querySelector('.tertiaryButton');
@@ -10,20 +9,17 @@ const allButtons = [primaryButton, secondaryButton, tertiaryButton];
 let resetButton;
 let gameOver = false;
 
-buttons.addEventListener('click', (event) => {  
-  if (!event.target.nodeName === 'BUTTON' || gameOver) {
-    return;
-  }
-  if(Number(event.target.id) === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    
-  } else {
-    lastResult.textContent = '!!!GAME OVER!!!' + ` THE WINNER BUTTON WAS ${randomNumber}!`;
-    lastResult.style.backgroundColor = 'red';
-  }
-  setGameOver();
-});
+function checkWinnerButton(idButton) {
+    if(Number(idButton) === randomNumber) {
+      lastResult.textContent = 'Congratulations! You got it right!';
+      lastResult.style.backgroundColor = 'green';
+      
+    } else {
+      lastResult.textContent = '!!!GAME OVER!!!' + ` THE WINNER BUTTON WAS ${randomNumber}!`;
+      lastResult.style.backgroundColor = 'red';
+    }
+    setGameOver();
+} 
 
 function setGameOver() {
     allButtons.forEach(button => button.disabled = true);
